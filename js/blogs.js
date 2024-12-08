e240264f385b3e6979beaa73db6164c62cb60832
imranhossen21 <imranhossen0586@gmail.com>
2024-12-08 19:04:12 +0600

    done

---
+++ js/blogs.js
@@ -0,0 +1,4 @@
+const blog = document.getElementById('blog-btn').addEventListener('click', function () {
+    location.href = "./blogs.html"
+
+});
---
+++ js/utility.js
@@ -0,0 +1,14 @@
+
+// Common Function 
+
+function getInputValueById(id) {
+    const inputValue = parseFloat(document.getElementById(id).value);
+    return inputValue;
+
+
+};
+
+function getTextValueById(id) {
+    const textValue = parseFloat(document.getElementById(id).innerText);
+    return textValue;
+}
